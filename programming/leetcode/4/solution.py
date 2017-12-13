92964a4254e5c50509a90724dee2941fd8666c00
Junlong Gao <junlong-gao@users.noreply.github.com>
2017-12-12 20:09:28 -0800

    lc4 AC

--- programming/leetcode/4/solution.py
+++ programming/leetcode/4/solution.py
@@ -21,3 +21,15 @@ def find_kth(a, b, k):
     if a[n - 1] <= b[l - 1]:
         return find_kth(a[n:], b, k - n)
     return find_kth(a, b[l:], k - l)
+
+class Solution(object):
+    def findMedianSortedArrays(self, s1, s2):
+        """
+        :type s1: List[int]
+        :type s2: List[int]
+        :rtype: float
+        """
+        l = len(s1) + len(s2)
+        if l % 2 == 1:
+            return find_kth(s1, s2, l/2)
+        return (find_kth(s1, s2, (l - 1)/2) + find_kth(s1, s2, (l + 1)/2)) / 2.0
