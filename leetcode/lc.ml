4ce5856129e9ac310d9dfadebd9a9b2483623491
Junlong Gao <junlongg@vmware.com>
2017-12-17 11:55:30 -0800

    Polymorphic Variant - A polymorphic variant is a variant that allow more disjoint types to   be glued. - An open polymorphic variant B is a subtype of A, having sig B < A - A code taking polymorphic variant pattern matching B, C, D   has input type A, which contains at least subtypes B, C, D... - Using polymorphic variant can express the type and subtyping in a   more flexible manner, and the code that operate on subtypes(color_to_int)   does not have to be changed for the general type   (extended_color_to_int). - Code that operate on general type can add a "narrowing case" for a new   subtype so the code on general type(extended_color_to_int) does not   have to handle all subtype cases(color_to_int). - Overall the flexibility increase the code reusing. - The con:   - Complexity   - Flexibility -> less strict semantic constraint   - Efficiency.

--- leetcode/lc.ml
+++ leetcode/lc.ml
@@ -56,15 +56,69 @@ end = struct
       ignore(in_order ~tree:left ~visitor:f);
       ignore(in_order ~tree:right ~visitor:f);
       ()
+
+  let%expect_test "bst" =
+    let root = Leaf
+    in
+    let root = insert ~tree:root ~to_insert:1
+    in
+    let root = insert ~tree:root ~to_insert:2
+    in
+    in_order ~tree:root ~visitor:(fun v -> Printf.printf "%d " v);
+    [%expect {|1 2|}]
 end
 
-let%expect_test "bst" =
-  let open Bst in
-  let root = Leaf
-  in
-  let root = insert ~tree:root ~to_insert:1
-  in
-  let root = insert ~tree:root ~to_insert:2
-  in
-  in_order ~tree:root ~visitor:(fun v -> Printf.printf "%d " v);
-  [%expect {| 1 2|}]
+module PolyVariant = struct
+  type basic_color =
+    [`Black | `Blue | `White]
+
+  type color =
+    [ `Basic of basic_color * [`Bold | `Regular]
+    | `Gray  of int
+    | `RGB   of int * int * int]
+
+  let basic_color_to_int = function
+    | `Black -> 0
+    | `Blue  -> 1
+    | `White -> 2
+
+  let color_to_int = function
+    | `Basic (c, w) ->
+      let w_name = match w with
+        | `Bold -> 100
+        | `Regular -> 200
+      in
+      w_name + (basic_color_to_int c)
+    | `Gray (v) -> v
+    | `RGB (x, y, z) -> 16 + z + y * 6 + x * 36
+
+  type extended_color =
+    [ color
+    | `RGBA of int * int * int * int]
+
+  let extended_color_to_int = function
+    | `RGBA (r, g, b, a) -> 256 * a + b * 6 + g * 36 + r * 216
+    (* narrowring *)
+    | (`Basic _ | `RGB _ | `Gray _) as color -> color_to_int color
+
+  let%expect_test "colors" =
+    ignore(
+      ignore(print_string "\nbasic color to int \n");
+      List.iter ~f:(fun c ->
+          Printf.printf "%d " (basic_color_to_int c)
+        ) [`Black; `Blue; `Blue];
+
+      ignore(print_string "\ncolor to int \n");
+      List.iter ~f:(fun c ->
+          Printf.printf "%d " (color_to_int c)
+        ) [`Basic(`Black, `Bold);
+           `Basic(`Blue, `Regular);];
+      ignore(print_string "\nextended color to int \n");
+      List.iter ~f:(fun c ->
+          Printf.printf "%d " (extended_color_to_int c)
+        ) [`RGBA(1, 2, 3, 4);
+           `Basic(`Blue, `Regular);];
+    );
+    [%expect {|
+   |}]
+end
