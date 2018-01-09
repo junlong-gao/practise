0a88816f04c54fa7726885d9eaf00fecd5839d1f
Junlong Gao <junlong-gao@users.noreply.github.com>
2018-01-08 22:00:50 -0800

    Update 727.cpp

--- code_practise/leetcode/727.cpp
+++ code_practise/leetcode/727.cpp
@@ -50,6 +50,40 @@ public:
 };
 
 /*
+Classic DP.
+
+let dp[i][j] =
+| T[0,j) is a subsequence of S[0, i) -> the largest index k such that S[k, i) has T[0, j) as subsequence.
+| _                                  -> -1
+
+Then
+dp[i][j] =
+match i with
+| 0     -> when i == 0, S[0, 0) is empty, and only empty can be a subsequence of an empty string:
+           dp[0][j] = j == 0? 0; -1;
+| i < j -> -1, as T[0, j) is longer than S[k, i)
+| _     -> match j with
+          | 0 -> T[0, 0) is empty, and is a subsequence of any string, so dp[i][0] = i - 1 (recall we track largest)
+          | 1 -> T[0, 1) has one character T[0], so we check
+                 match T[0] with
+                 | S[i - 1]  -> i - 1
+                 | _         -> dp[i-1][j] because we just consider substring S[0, i-1) to find the largest index k
+                                such that S[k] = T[0]
+          | _ -> match T[j - 1] with
+                 | S[i - 1] -> two possibilities to construct a solution: dp[i-1][j-1] or dp[i - 1][j], take the max
+                 | _        -> only one way: dp[i - 1][j]
+
+This dp recursive relation can be easily calculated by filling the whole table dp[0...n][0...m] 
+where n = |S|, m = |T|, basically each cell depends on its immeidate above cell (dp[i-1][j]) 
+and the left diag (dp[i-1][j-1]). So classic left to right downwards direction.
+
+When filling the table, we keep looking at the solution of dp[i][m] for all i, as this 
+is the index of S such that S[dp[i][m], i) contains T as a substring, and for each 
+of these dp[i][m], we keep track of the minimal window (window length = i - dp[i][m]),
+as well as the beginning index dp[i][m] of the window.
+
+The solution is the minimal window and the beginning index dp[i][m] of the window.
+
   cout << s.minWindow("abcdebdde", "bde") << endl;
   cout << s.minWindow("a", "a") << endl;
   cout << s.minWindow("aa", "a") << endl;
