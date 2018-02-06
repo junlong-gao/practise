ca01d4e996e6abda531f5ad659087fad312ed236
Junlong Gao <junlongg@vmware.com>
2018-02-05 22:38:03 -0800

    rec02

--- real_street_ocaml/project_cornell/rec02.ml
+++ real_street_ocaml/project_cornell/rec02.ml
@@ -3,3 +3,9 @@ let inc x = x + 1
 let rec fact n = match n with
              | 0 -> 1
              | k -> k * (fact (k - 1))
+
+let id x = x
+
+let divide ?test:(arg=false) ~num:(x:float) ~denum:(y:float) = if arg then 0.0 else (x /. y)
+
+let ($$) x y = (x +. y) /. 2.0
