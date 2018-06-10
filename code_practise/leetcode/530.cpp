a227a44448f300470a7439553a7256005447add1
Junlong Gao <junlong-gao@users.noreply.github.com>
2018-06-10 16:15:44 -0700

    Create 530.cpp

---
+++ code_practise/leetcode/530.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        TreeNode *cur = root;
+	int prev = INT_MAX;
+	int best = INT_MAX;
+	while (cur) {
+		if (cur->left == nullptr) {
+			if (prev == INT_MAX) {
+				prev = cur->val;
+			} else {
+				assert(cur->val >= prev);
+				best = min(best, cur->val - prev);
+			}
+			cur = cur->right;
+		} else {
+			TreeNode *pred = cur->left;
+			while(pred->right && pred->right != cur) {
+				pred = pred->right;
+			}
+			if (pred->right == nullptr) {
+				pred->right = cur;
+				cur = cur->left;
+			} else {
+				pred->right = nullptr;
+			if (prev == INT_MAX) {
+				prev = cur->val;
+			} else {
+				assert(cur->val >= prev);
+				best = min(best, cur->val - prev);
+			}
+				cur = cur->right;
+			}
+		}
+	
+	}
+	return best;
+    }
+};
+
+
