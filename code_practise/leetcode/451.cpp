0cb626debe8752cea78d88b4f6311f877941327a
Junlong Gao <junlong-gao@users.noreply.github.com>
2018-06-11 23:03:07 -0700

    Update 451.cpp

--- code_practise/leetcode/451.cpp
+++ code_practise/leetcode/451.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> f;
-        for (auto char c : s) {
-            f[c]++;
+        vector<pair<char, int>> alpha(256);
+        for (int i = 0; i < 256; ++i) {
+            alpha[i].first = i;
+            alpha[i].second = 0;
         }
-        sort(s.begin(), s.end(), [&](const char l, const char r) {
-            return f[l] > f[r];
+        for (char c : s) {
+            alpha[c] = pair<char, int>{alpha[c].first, alpha[c].second + 1};
+        }
+        sort(alpha.begin(), alpha.end(), [&](const pair<char, int> l,
+                                             const pair<char, int> r){
+           return l.second > r.second; 
         });
-        return s;
+        string ret;
+        for (int i = 0; i < 256; ++i) {
+            for (int j = 0; j < alpha[i].second; ++j) {
+                ret += alpha[i].first;
+            }
+        }
+        return ret;
     }
 };
