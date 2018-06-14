01be3d9185eb57c247b4dcfd7d137767ce97c81e
Junlong Gao <junlong-gao@users.noreply.github.com>
2018-06-13 22:19:45 -0700

    Update 844.cpp

--- code_practise/leetcode/844.cpp
+++ code_practise/leetcode/844.cpp
@@ -4,7 +4,7 @@ public:
     bool backspaceCompare(string S, string T) {
         int i = S.size() - 1, j = T.size() - 1;
         int Sb = 0, Tb = 0;
-        while (i >= 0 && j >= 0) {
+        while (i >= 0 || j >= 0) {
             while (i >= 0 && (Sb || S[i] == '#')) {
                 if (S[i] == '#') {
                     Sb++;
@@ -24,8 +24,11 @@ public:
             if (i >= 0 && j >= 0) {
                 if (S[i] != T[j]) return false;
                 i--; j--;
-            }
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }    
         }
-        return i < 0 && j < 0;
+
+        return true;
     }
 };
