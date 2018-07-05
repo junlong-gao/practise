45bc0db8b6c113193f0b2eb81aef57ef76c80225
Junlong Gao <junlonga@umich.edu>
2018-07-05 00:14:12 +0000

    shuffling

--- code_practise/leetcode/main.cpp
+++ code_practise/leetcode/main.cpp
@@ -48,7 +48,24 @@ void test() {
 }
 }
 
+namespace ss864 {
+#include "864.cpp"
+   void test() {
+      int N = 20; vector<int> l {0, 1, 2,4,5,8,9,10,15,16,19, 19, 20, 21, 22, 22}; 
+      unordered_map<int, int> hist;
+      Solution s(N, l);
+      
+      for (int i = 0; i < 1000; ++i) {
+         hist[s.pick()]++;
+      }
+      cout << l << endl;
+      cout << hist << endl;
+      return;
+   }
+}
+
 int main() {
    ss68::test();
+   ss864::test();
    cout << "Passed..." << endl;
 }
