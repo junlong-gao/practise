56529f5ae80b8e99ae4fab9af4ed5ca761490063
bigFattyBoi <junlong-gao@users.noreply.github.com>
2019-07-01 01:24:53 -0700

    Update 862.cpp

--- code_practise/leetcode/862.cpp
+++ code_practise/leetcode/862.cpp
@@ -1,39 +1,54 @@
 class Solution {
+    /*
+    Let opt[j] be the largest index i such that P[j] - P[i] >= K,
+    where P is the prefix sum (hence P[j] - P[i] is sum [i...j-1], of length
+    j - i).
+    Let mq[i] be the candidate set for opt[i+1], such that
+    1. it is increasing (x < y in mq[i] => P[x] < P[y])
+    2. opt[i+1], if exists and is better than all previous solutions, 
+       must be in mq[i] (thus not empty), and
+       is the largest element x such that P[i + 1] - P[x] >= K
+       
+     mq[i] is maintained (into mq[i+1]) via poping matched front (smaller
+     size) of the set whenever P[i + 1] - mq.front() >= K and poping back 
+     whenever P[i + 1] >= mq.back();
+     
+     proof:
+     mq[0], trivial
+     assume after maintaining, the invariant no longer holds for mq[i + 1].
+     i.e. x = opt[i + 2], assmuing exists and better than previous solutions, 
+     is not in mq[i + 1]. note each element is enqueued exactly once,
+     to lose x is to pop it in the front or back in previous maintaining.
+     1. x is discarded during some previous pop front:
+        contradiction: as x is discarded in the previous pop front, the
+        index difference between x and i' is strictly better than x and i + 2
+        for i' < i + 2
+     2. x is discarded during some previous pop back:
+        contradiction: as P[some previous mq.back()] <= x, 
+        if P[some previous mq.back()] is in mq[i+1], then this is a 
+        strictly batter solution. otherwise 
+        mq[i+1].back() <= P[some previous mq.back()] is also a strictly better
+        solution.
+    */
 public:
     int shortestSubarray(vector<int>& A, int K) {
-        vector<int> sum{0};
-        for (auto e : A) {
-            sum.push_back(sum.back() + e);
+        deque<int> mq;
+        vector<int> P {0};
+        for (int i = 0; i < A.size(); ++i) {
+            P.push_back(A[i] + P.back());
         }
-        deque<int> cand;
-        int best = INT_MAX;
-        for (int i = 0; i < sum.size(); ++i) {
-            while(cand.size() && sum[i] - sum[cand.front()] >= K) {
-                best = min(best, i - cand.front());
-                cand.pop_front();
+        int ans = P.size() + 1;
+        for (int i = 0; i < P.size(); ++i) {
+            while (mq.size() && P[i] - P[mq.front()] >= K) {
+                ans = min(ans, i - mq.front());
+                mq.pop_front();
             }
-            
-            while (cand.size() && sum[cand.back()] >= sum[i]) {
-                cand.pop_back();
+            while (mq.size() && P[i] <= P[mq.back()]) {
+                mq.pop_back();
             }
-            cand.push_back(i);
+            mq.push_back(i);
         }
-        return best == INT_MAX ? -1 : best;
+        
+        return ans == P.size() + 1 ? -1 : ans;
     }
 };
-
-/*
-[0, 2, 1, 3]
-{0}
-{0, 1}
-{0, 2}
-{2, 3} best = 3 - 0 = 3
-
- 0  1  2  3  4  5
-[0, 1, 3, 6, 3, 4]
-K = 3
-{0}
-{0, 1}
-{1, 2} best = 2
-{3} best = 1
-*/
