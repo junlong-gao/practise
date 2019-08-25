dada44ec6f39a170202de71dcaad8a8329f32616
bigFattyBoi <junlong-gao@users.noreply.github.com>
2019-08-25 13:28:11 -0700

    Update 499.cpp

--- code_practise/leetcode/499.cpp
+++ code_practise/leetcode/499.cpp
@@ -1,56 +1,61 @@
-/*
-This can be done in Dijstra's by breaking ties using lex order of the path to the node.
-On the other hand, the search space is a dag
-*/
 class Solution {
-	vector<vector<string>> paths;
-	vector<vector<int>> dist;
-	vector<vector<int>> g;
-	int dx[4] = {0, 1, 0, -1};
-	int dy[4] = {1, 0, -1, 0};
-	string dir[4] = {"r", "d", "l", "u"};
-	int n, m;
-	int destx; int desty;
-	void dfs(int x, int y) {
-		for (int i = 0; i < 4; ++i) {
-			int nx = x; int ny = y;
-			while (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-			       g[nx][ny] == 0 && !(nx == destx && ny == desty)) {
-				nx += dx[i]; ny += dy[i];
-			}
-
-			if (!(nx == destx && ny == desty)) {
-				nx -= dx[i]; ny -=dy[i];
-				if (nx == x && ny == y) continue;
-			} 
-
-			int newDist = dist[x][y] + abs(nx - x) + abs(ny - y);
-			if (newDist > dist[nx][ny]) continue;
-			string newPath = paths[x][y] + dir[i];
-			if (newPath < paths[nx][ny] || newDist < dist[nx][ny]) {
-				paths[nx][ny] = newPath;
-				dist[nx][ny] = newDist;
-				if (!(nx == destx && ny == desty)) {
-					dfs(nx, ny);
-				}
-			}
-		}
-	}
 public:
     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
-            n = maze.size(); if (n == 0) return "";
-	m = maze[0].size();
-	const int INF = n * m + 1;
-	paths.resize(n, vector<string>(m));
-	dist.resize(n, vector<int>(m, INF));
-	swap(maze, g);
-	destx = hole[0]; desty = hole[1];
-	
-	dist[ball[0]][ball[1]] = 0;
-	paths[ball[0]][ball[1]] = "";
-	dfs(ball[0], ball[1]);
+        const int n = maze.size(); if (n == 0) return "";
+        const int m = maze[0].size();
+        const int INF = n * m + 1;
+        const int dx[4] = {0, 1, 0, -1};
+        const int dy[4] = {1, 0, -1, 0};
+        const string dir[4] = {"r", "d", "l", "u"};
+        using d_t = pair<int, string>;
+        struct n_t {
+          int x; int y;
+          d_t d;
+        };
+        auto cd = [](const d_t &l, const d_t &r) {
+            if (l.first != r.first) {
+                return l.first < r.first;
+            }
+            return l.second < r.second;
+        };
+        auto cn = [&cd](const n_t &l, const n_t &r) {
+            return cd(l.d, r.d);
+        };
+        
+        vector<vector<d_t>> d(n, vector<d_t>(m, make_pair(INF, "")));
+        priority_queue<n_t, vector<n_t>, decltype(cn)> q(cn);
+        
+        d[ball[0]][ball[1]] = make_pair(0, "");
+        q.push((n_t){ball[0], ball[1], make_pair(0, "")});
+        
+        while (!q.empty()) {
+            n_t t = q.top(); q.pop();
+            if (t.d != d[t.x][t.y]) continue;
+            
+            for (int i = 0; i < 4; ++i) {
+                int nx = t.x; int ny = t.y;
+                while (nx >= 0 && nx < n && 
+                       ny >= 0 && ny < m &&
+                       maze[nx][ny] == 0 && 
+                       !(nx == hole[0] && ny == hole[1])) {
+                    nx += dx[i]; ny += dy[i];
+                }
 
-	return paths[hole[0]][hole[1]] == "" ? "impossible" : paths[hole[0]][hole[1]];
+                if (!(nx == hole[0] && ny == hole[1])) {
+                    nx -= dx[i]; ny -=dy[i];
+                    if (nx == t.x && ny == t.y) continue;
+                }
+                
+                n_t nn {nx, ny, 
+                        make_pair(t.d.first + abs(nx - t.x) + abs(ny - t.y), 
+                                  t.d.second + dir[i])};
+                if (cd(nn.d, d[nx][ny])) {
+                    d[nx][ny] = nn.d;
+                    q.push(nn);
+                }
+            }
+        }
+        
+        return d[hole[0]][hole[1]].second == "" ? "impossible" : d[hole[0]][hole[1]].second;
     }
 };
-
