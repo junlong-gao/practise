b6d6a01fd60537de5b9d3a4b8b42dde8bcb1cee1
bigFattyBoi <junlong-gao@users.noreply.github.com>
2019-08-30 11:28:39 -0700

    Update Solution.h

--- code_practise/leetcode/295/Solution.h
+++ code_practise/leetcode/295/Solution.h
@@ -1,69 +1,40 @@
-//
-// Created by junlong on 8/3/16.
-//
-
-#ifndef PRACTISE_SOLUTION_H
-#define PRACTISE_SOLUTION_H
-#include <queue>
-#include <vector>
-
-using namespace std;
-
-//two heaps: up is larger or equal to lower
-// use int to keep track of size
-
 class MedianFinder {
-    int upSize;
-    int lowSize;
-    priority_queue<int, std::vector<int>, std::less<int>> up;
-    priority_queue<int, std::vector<int>, std::greater<int>> lower;
-public:
-    MedianFinder(){
-        upSize = lowSize = 0;
+    priority_queue<int, vector<int>, std::greater<int>> upper;
+    priority_queue<int, vector<int>, std::less<int>> lower;
+
+    template<typename T1, typename T2>
+    void balance(T1& larger, T2& smaller){
+        while(!larger.empty() && larger.size() - smaller.size() >= 2){
+            auto top = larger.top(); larger.pop();
+            smaller.push(top);
+        }
     }
+public:
 
     // Adds a number into the data structure.
     void addNum(int num) {
-        if(!lower.empty() && num > lower.top()){
-            lower.push(num);
-            lowSize++;
-        }else if (!up.empty() && num <= up.top()){
-            up.push(num);
-            upSize++;
+        if(lower.empty() || num <= lower.top()){
+			lower.push(num);
         }else{
-            lower.push(num);
-            lowSize++;
+            upper.push(num);
         }
-        if(lowSize - upSize == 2){
-            int top = lower.top();
-            lower.pop();
-            lowSize --;
-            up.push(top);
-            upSize++;
+        
+        if((int)(lower.size() - upper.size()) >= 2){
+            balance(lower, upper);
         }
-        if(upSize - lowSize == 2){
-            int top = up.top();
-            up.pop();
-            upSize --;
-            lower.push(top);
-            lowSize++;
+        
+        if((int)(lower.size() - upper.size()) <= -2){
+            balance(upper, lower);
         }
-
     }
 
     // Returns the median of current data stream
     double findMedian() {
-        if(upSize == lowSize){
-            return ((double)up.top() + (double)lower.top())/2;
-        }else{
-            return lowSize > upSize? lower.top(): up.top();
+        if(upper.size() != lower.size()){
+            return upper.size() > lower.size() ? 
+                      (double)upper.top() :
+                      (double)lower.top();
         }
+        return (double)(upper.top() + lower.top())/2.0;
     }
 };
-
-// Your MedianFinder object will be instantiated and called as such:
-// MedianFinder mf;
-// mf.addNum(1);
-// mf.findMedian();
-
-#endif //PRACTISE_SOLUTION_H
