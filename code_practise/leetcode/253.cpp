ce5185d1bd4a1f3b82ddb7d8374271ad0205becf
cat_mint <junlong-gao@users.noreply.github.com>
2019-09-08 15:35:34 -0700

    Update 253.cpp

--- code_practise/leetcode/253.cpp
+++ code_practise/leetcode/253.cpp
@@ -1,31 +1,24 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
-        map<int, int> p;
-        for (const auto & i : intervals) {
-            p[i.start] += 1;
-            p[i.end] -= 1;
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        vector<int> st, et;
+        for (auto & i : intervals) {
+            st.push_back(i[0]);
+            et.push_back(i[1]);
         }
-
-        int cur = 0; int best = 0;
-        for (auto it = p.begin(); it != p.end(); ++it) {
-            cur += it->second;
-            best = max(best, cur);
+        
+        sort(st.begin(), st.end());
+        sort(et.begin(), et.end());
+        int sp = 0; int ep = 0;
+        int ret = 0;
+        while (sp < st.size()) {
+            if (st[sp] < et[ep]) {
+                ret++;
+            } else {
+                ep++;
+            }
+            sp++;
         }
-        return best;
+        return ret;
     }
 };
-
-/*
-cf. Skyline
-cf. count rect area
-*/
