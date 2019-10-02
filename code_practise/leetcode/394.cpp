d5b1d65107d5fefa71aa0dba6a2653b0d009148b
cat_mint <junlong-gao@users.noreply.github.com>
2019-10-01 19:03:12 -0700

    Update 394.cpp

--- code_practise/leetcode/394.cpp
+++ code_practise/leetcode/394.cpp
@@ -1,92 +1,36 @@
-/*
-
-encoded = expr (#pop) encoded (#pop, concat, push)
-        | eplison
-
-expr = <literal> (# push literal)
-     | <number> [ encoded ] (# pop, repeat for number times, push)
-
-literal -> alphabets
-number -> digit*
-
-
-example:
-3[a2[c]]
-accaccacc
-*/
-
 class Solution {
-  int lookahead = 0;
-  string str;
-  vector<string> stack;
-  void encoded();
-  void expr() {
-    assert(lookahead != str.size());
-    if (!std::isdigit(str[lookahead])) {
-      string v;
-      while (lookahead < str.size() &&
-             (std::islower(str[lookahead]) || std::isupper(str[lookahead]))) {
-        v.push_back(str[lookahead++]);
-      }
-      stack.push_back(v);
-      return;
-    } else {
-      string rep;
-      while (std::isdigit(str[lookahead])) {
-        rep.push_back(str[lookahead++]);
-      }
-      int n_rep = stoi(rep);
-
-      assert(str[lookahead] == '[');
-      lookahead++;
-
-      encoded();
-      assert(str[lookahead] == ']');
-      lookahead++;
-      string output = std::move(stack.back());
-      stack.back() = "";
-      for (int i = 0; i < n_rep; ++i) {
-        stack.back() += output;
-      }
+    
+    string decode(int& start, string& s) {
+        if (start == s.size() || s[start] == ']') {
+            return "";
+        } else if (std::isdigit(s[start])) {
+            int cur = start;
+            while (start < s.size() && std::isdigit(s[start])) {
+                start++;
+            }
+            int n_rep = stoi(s.substr(cur, start - cur));
+            assert(s[start] == '['); start++;
+            string temp = decode(start, s);
+            assert(s[start] == ']'); start++;
+                
+            string ret;
+            for (int i = 0; i < n_rep; ++i) {
+                ret += temp;
+            }
+            return ret + decode(start, s);
+        } else {
+            int cur = start; assert(std::isalpha(s[start]));
+            while (start < s.size() && std::isalpha(s[start])) {
+                start++;
+            }
+            string t = s.substr(cur, start - cur);
+            return  t + decode(start, s);
+            // return s.substr(cur, start - cur) + decode(start, s); is wrong. Why?
+        }
     }
-  }
-
- public:
-  string decodeString(string s) {
-    str = std::move(s);
-    lookahead = 0;
-    encoded();
-    return stack.back();
-  }
+public:
+    string decodeString(string s) {
+        int start = 0;
+        return decode(start, s);
+    }
 };
-
-void Solution::encoded() {
-  if (lookahead == str.size() || str[lookahead] == ']') {
-    stack.push_back("");
-    return;
-  }
-
-  expr();
-  string out1 = std::move(stack.back());
-  stack.pop_back();
-
-  encoded();
-  string out2 = std::move(stack.back());
-  stack.pop_back();
-
-  stack.push_back(out1 + out2);
-  return;
-}
-
-/*
-aa
-
-3[aa]
-
-bb3[cc]
-
-a3[4[b5[d]]] ->
-abdddddbdddddbdddddbdddddbdddddbdddddbdddddbdddddbdddddbdddddbdddddbddddd
-
-
-*/
