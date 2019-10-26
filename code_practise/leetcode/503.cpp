21e27ca17b3f2c551fe5d540a258606121a9ac5a
cat_mint <junlong-gao@users.noreply.github.com>
2019-10-26 15:30:27 -0700

    Update 503.cpp

--- code_practise/leetcode/503.cpp
+++ code_practise/leetcode/503.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<pair<int, int>> s;
+        vector<int> s;
         vector<int> ret(nums.size(), -1);
         for (int i = 0; i < 2 * nums.size(); i++) {
-            while (s.size() && nums[i % (nums.size())] > s.back().second) {
-                ret[s.back().first] = nums[i % (nums.size())];
+            while (s.size() && nums[i % (nums.size())] > nums[s.back()]) {
+                ret[s.back()] = nums[i % (nums.size())];
                 s.pop_back();
             }
             if (i < nums.size()) {
-                s.push_back(make_pair(i, nums[i]));
+                s.push_back(i);
             }
         }
         
