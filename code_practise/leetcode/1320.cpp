1b47f2b319823b55e9b582db1789d711de2a82f3
Junlong Gao <junlonga@umich.edu>
2020-01-13 23:55:11 -0800

    R  ../Vagrantfile -> ../../Vagrantfile  M ../leetcode/1320.cpp A  2991.cpp

--- code_practise/leetcode/1320.cpp
+++ code_practise/leetcode/1320.cpp
@@ -1,45 +1,30 @@
+auto f = []() {
+    std::ios::sync_with_stdio(false);cin.tie(NULL); return 0;
+}();
+
 class Solution {
-    int infty = INT_MAX / 2;
-    unordered_map<char, pair<int, int>> table;
     int dist(char c1, char c2) {
-        return abs(table[c1].first - table[c2].first) + abs(table[c1].second - table[c2].second);
+        int xc1 = c1 / 6; int yc1 = c1 % 6;
+        int xc2 = c2 / 6; int yc2 = c2 % 6;
+        return abs(xc1 - xc2) + abs(yc1 - yc2);
     }
-    vector<vector<vector<int>>> dp;
-    int cost(const string w, int K, char c1, char c2) {
-        if (K <= 1) return 0;
-        if (dp[K][c1][c2] >= 0) return dp[K][c1][c2];
-        int &ret = dp[K][c1][c2];
-        if (c1 == w[K - 1]) {
-            ret = min(
-                cost(w, K - 1, w[K - 2], c2) + dist(w[K - 2], w[K - 1]),
-                cost(w, K - 1, c1, w[K - 2]) + dist(w[K - 2], c2)
-            );
-            
-            return ret;
-        } else if (c2 == w[K - 1]) {
-            ret = min(
-                cost(w, K - 1, c1, w[K - 2]) + dist(w[K - 2], w[K - 1]),
-                cost(w, K - 1, w[K - 2], c2) + dist(w[K - 2], c1)
-            );
-            
-            return ret;
-        } else {
-            return ret = infty;
-        }
-    }
+    
 public:
     int minimumDistance(string word) {
-        for (char c = 'A'; c <= 'Z'; ++c) {
-            table[c] = make_pair((c - 'A') / 6, (c - 'A') % 6);
-        }
-        dp.resize(word.size() + 1,
-                 vector<vector<int>>(256, vector<int>(256, -1)));
-        int ret = infty;
-        for (char c1 = 'A'; c1 <= 'Z'; ++c1) {
-            for (char c2 = 'A'; c2 <= 'Z'; ++c2 ) {
-                ret = min(ret, cost(word, word.size(), c1, c2));
+        array<array<int, 26>, 2> dp;
+
+        int ret = INT_MAX;
+        for (int i = 0; i < word.size(); ++i) {
+            for (int c = 0; c < 26; ++c) {
+                if (i == 0) { dp[i][c] = 0; continue; }
+                dp[i & 1][c] = min(dp[(i + 1) & 1][c] + dist(word[i - 1] - 'A', word[i] - 'A'),
+                               dp[(i + 1) & 1][word[i] - 'A'] + dist(word[i - 1] - 'A', c));
+                if (i == word.size() - 1) {
+                    ret = min(ret, dp[i & 1][c]);
+                }
             }
         }
+
         return ret;            
     }
 };
