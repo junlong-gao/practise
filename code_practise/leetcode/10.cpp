721bce49cc340540d59161d0e1676ff4dd0445a4
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-02 15:13:50 -0800

    Update 10.cpp

--- code_practise/leetcode/10.cpp
+++ code_practise/leetcode/10.cpp
@@ -1,27 +1,31 @@
-//
-// Created by Junlong Gao on 8/2/16.
-//
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+       vector<vector<bool>> dp(s.size() + 1, vector<bool>(p.size() + 1, false));
 
-#include "Solution.h"
-#include <vector>
-#include <string>
-#include <assert.h>
-
-using namespace std;
-
-
-int main(){
-    Solution testObj;
-    string s = "aa";
-    string p = "aa";
-
-    assert(testObj.isMatch(s, p));
-    assert(testObj.isMatch("aa", ".*"));
-    assert(testObj.isMatch("aaa", "a.*"));
-    assert(testObj.isMatch("aa", "a*"));
-    assert(testObj.isMatch("ab", "a.*"));
-    assert(testObj.isMatch("ab", "ab*"));
-    assert(testObj.isMatch("aab", "c*a*b"));
-    assert(!testObj.isMatch("abcd", "d*"));
-
-}
+       for (int i = 0; i <= s.length(); ++i) {
+          for (int j = 0; j <= p.length(); ++j) {
+             if (i == 0 && j == 0) {
+                dp[i][j] = true;
+             } else if (j == 0 && i > 0) {
+                dp[i][j] = false;
+             } else if (i == 0 && j > 0) {
+                if (p[j-1] == '*') {
+                   dp[i][j] = dp[i][j-2];
+                } else {
+                   dp[i][j] = false;
+                }
+             } else {
+                if (p[j-1] == '*') {
+                   dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (p[j-2]=='.' || s[i-1] == p[j-2]));
+                } else if (p[j-1]=='.'){
+                   dp[i][j]=dp[i-1][j-1];
+                } else {
+                   dp[i][j]=dp[i-1][j-1]&&(s[i-1]==p[j-1]);
+                }
+             }
+          }
+       }
+       return dp[s.size()][p.size()];
+    }
+};
