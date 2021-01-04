96ca7d2d2a6f2838849b0f6b33216e3ff634af7f
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-03 18:42:38 -0800

    Update 53.cpp

--- code_practise/leetcode/53.cpp
+++ code_practise/leetcode/53.cpp
@@ -1,34 +1,18 @@
-//
-//  main.cpp
-//  Maximum Subarray
-//
-//  Created by John on 9/26/15.
-//  Copyright © 2015 John. All rights reserved.
-//
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
-public: int maxSubArray(vector<int>& nums) { 
-	int s[nums.size()]; 
-	int max = s[0] = nums[0]; 
-	for (int i = 1; i < nums.size(); i++) {
-		s[i] = s[i-1] > 0 ? (nums[i] + s[i-1]) : nums[i]; 
-		max = std::max(max, s[i]); 
-	}
-	return max;
-}
-};
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> pfx{0};
+        for (int i = 0; i < nums.size(); ++i) {
+           pfx.push_back(pfx.back() + nums[i]);
+        }
 
-int main(int argc, const char * argv[]) {
-	vector<int> test = {-2,1,-3,4,-1,2,1,-5,4};
-	vector<int> test2 = {-2,1};
-	vector<int> test3 = {-2};
-	vector<int> test4 = {1,-2};
-	vector<int> test5 = {1,2};
-	Solution mysolution;
-	mysolution.maxSubArray(test5);
-    return 0;
-}
+        int runMin = pfx[0];
+        int ret = pfx[1];
+        for (int i = 1; i < pfx.size(); ++i) {
+           ret = max(ret, pfx[i] - runMin);
+           runMin = min(runMin, pfx[i]);
+        }
+
+        return ret;
+    }
+};
