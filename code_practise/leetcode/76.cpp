4c4434074a1379e22f355b71fe3d032b78fe4db2
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-04 18:40:48 -0800

    Update 76.cpp

--- code_practise/leetcode/76.cpp
+++ code_practise/leetcode/76.cpp
@@ -1,67 +1,47 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include <vector>
-#include <deque>
-#include <algorithm>
-#include <unordered_map>
-#include <unordered_set>
+auto f = [](){
+    ios::sync_with_stdio(false);cin.tie(NULL);return 0;
+}();
 
-
-namespace{
-	using namespace std;
-    class Solution {
-    public:
-        string minWindow(string s, string t) {
-            unordered_map<char, int> copy, table;
-            for(int i = 0; i < t.length(); ++i){
-                copy[t[i]]++;
+class Solution {
+#define Vec vector
+public:
+    string minWindow(string s, string t) {
+        Vec<int> p(256, 0); Vec<int> q(256, 0);
+        int toFind = 0;
+        for (int i = 0; i < t.size(); ++i) {
+            if (q[t[i]] == 0) {
+                toFind++;
             }
-            table = copy;
-            int i = 0, j = 0;
-            int tofind = t.length();
-            int cur = numeric_limits<int>::max();
-            string ans = "";
-            while(i < s.length()){
-                while(tofind > 0 && j < s.length()){
-                    if(copy.find(s[j]) != copy.end()){
-                        //cout << "increase j" << endl;
-                        if(copy[s[j]]>0) tofind--;
-                        copy[s[j]]--;
-                        //cout << s[j] << " " << copy[s[j]] << endl;
-                        
-                    }
-                    ++j;
-                }
-                while(tofind == 0 && i < s.length()){
-                    if(copy.find(s[i]) != copy.end()){
-                        if(copy[s[i]] != table[s[i]]){
-                            //cout << "increase i" << endl;
-                            copy[s[i]]++;
-                            //cout << s[i] << " " << copy[s[i]] << endl;
-                            if(copy[s[i]] == 1){
-                                if(j - i < cur){
-                                    //cout << s[i] << " " << i << " " << j << endl;
-                                    cur = j - i;
-                                    ans = s.substr(i, j - i);
-                                }
-                                tofind++;
-                            }
-                        }
-                    }
-                    ++i;
-                }
-                if(j == s.length()) break;
-            }
-            return ans;
+            q[t[i]]++;
         }
-    };
-    
-    TEST_CASE("tests"){
-        Solution testObj;
-        SECTION("sample"){
-            REQUIRE(testObj.minWindow("aababa", "abb")=="bab");
-		}
-	}
-}
-
-
+        
+        int i = 0; int j = 0; int best = INT_MAX;
+        int reti, retj;
+        int found = 0;
+        while (j < s.size()) {
+            while (j < s.size() && found < toFind) {
+                p[s[j]]++;
+                j++;
+                if (p[s[j-1]] == q[s[j-1]]) {
+                    found++;
+                }
+            }
+            while (i < j && found == toFind) {
+                if (j - i < best) {
+                    best = j - i;
+                    reti= i;
+                    retj = j;
+                }
+                p[s[i]]--;
+                i++;
+                if (p[s[i-1]] < q[s[i-1]]) {
+                    found--;
+                }
+            }
+        }
+        if (best == INT_MAX) {
+            return "";
+        }
+        return s.substr(reti, retj - reti);
+    }
+};
