029e8368294e8dd53ea29a3b76f250fe68455e14
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-05 19:55:12 -0800

    Update 138.cpp

--- code_practise/leetcode/138.cpp
+++ code_practise/leetcode/138.cpp
@@ -1,21 +1,58 @@
-  RandomListNode *copyRandomList(RandomListNode *head) {
-    if (head == nullptr) return nullptr;
-    RandomListNode *l, *r;
-    for (l = head; l != nullptr; l = l->next->next) {
-      r = new RandomListNode(l->label);
-      r->next = l->next;
-      l->next = r;
-    }
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
     
-    for (l = head; l != nullptr; l = l->next->next) {
-      if (l->random) l->next->random = l->random->next;
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
-    
-    RandomListNode *ret = head->next;
-    for (l = head; l != nullptr; l = l->next) {
-      r = l->next;
-      l->next = r->next;
-      if (r->next) r->next = r->next->next;
+};
+*/
+/*
+Waven trick: meld 2 lists into 1 list (even, odd, even, odd ...)
+and set the random for odd nodes.
+Un-waven it, consider the last node case.
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) return nullptr;
+        Node *cur = head;
+        while (cur) {
+            Node *oldNext = cur->next;
+            cur->next = new Node(cur->val);
+            cur->next->next = oldNext;
+            cur = oldNext;
+        }
+        
+        cur = head;
+        while (cur) {
+            cur->next->random = cur->random;
+            if (cur->next->random) {
+                cur->next->random = cur->next->random->next;
+            }
+            cur = cur->next->next;
+        }
+        
+        cur = head;
+        Node *ret = cur->next;
+        while (cur) {
+            Node *oldNext = cur->next->next;
+            if (oldNext == nullptr) {
+                cur->next->next = nullptr;
+            } else {
+                cur->next->next = oldNext->next;
+            }
+            
+            cur->next = oldNext;
+            cur = cur->next;
+        }
+        return ret;
     }
-    return ret;
-  }
+};
