2b5c8aba99fe25860060d25d76ff43fa87b99108
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-07 18:59:21 -0800

    Update 362.cpp

--- code_practise/leetcode/362.cpp
+++ code_practise/leetcode/362.cpp
@@ -1,12 +1,11 @@
 class HitCounter {
-	struct Hit {
-		int t;
-		int count;
-		Hit (int t_, int count_) : t(t_), count(count_) {;}
-	};
-	deque<Hit> window;
-	int counter {0};
-	static constexpr int WINDOW_SIZE {300};
+    int total = 0;
+    const int fiveMin = 300;
+    struct ent_t {
+        int tt;
+        int count;
+    };
+    deque<ent_t> hits;
 public:
     /** Initialize your data structure here. */
     HitCounter() {
@@ -16,33 +15,28 @@ public:
     /** Record a hit.
         @param timestamp - The current timestamp (in seconds granularity). */
     void hit(int timestamp) {
-	counter ++;
-            if (window.empty() || window.back().t != timestamp) {
-		window.emplace_back(timestamp, 1);
-/*
-XXX2
-forget to early return here.
-or should just use explicit else
-	}
-	window.back().count++;
-*/
-	} else {
-		window.back().count++;
-	}
+        total++;
+        if (hits.size() && hits.back().tt == timestamp) {
+            hits.back().count++;
+        } else {
+            hits.push_back(ent_t{timestamp, 1});
+        }
     }
     
     /** Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity). */
     int getHits(int timestamp) {
-/*
-XXX1 this is wrong. as time as progressed to timestamp, not window.back().t
-so when called by timestamp > window.back().t you are wrong.
-            while (!window.empty() && window.back().t - window.front().t + 1 > WINDOW_SIZE) {
-*/
-            while (!window.empty() && timestamp - window.front().t + 1 > WINDOW_SIZE) {
-		counter -= window.front().count;
-		window.pop_front();
-	}
-	return counter;
+        while (hits.size() && (timestamp - hits.front().tt >= fiveMin)) {
+            total -= hits.front().count;
+            hits.pop_front();
+        }
+        return total;
     }
 };
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
