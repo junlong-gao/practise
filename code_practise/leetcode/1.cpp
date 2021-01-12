df7b94dab07f14d4081ef9040ed54ef942eda899
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-11 20:11:09 -0800

    Update 1.cpp

--- code_practise/leetcode/1.cpp
+++ code_practise/leetcode/1.cpp
@@ -1,27 +1,13 @@
-//
-// Created by Junlong Gao on 8/6/16.
-//
-
-#ifndef PRACTISE_SOLUTION_H
-#define PRACTISE_SOLUTION_H
-#include <vector>
-#include <unordered_map>
-using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int, int> m;
-       vector<int> ret;
-       for(int i = 0; i < nums; ++i){
-           if(m.find(target - nums[i]) != m.end()){
-               return {m[target-nums[i]], i};
-           }else{
-               m[nums[i]] = i;
-           }
-       }
-       return ret;
+        unordered_map<int, int> ret;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (ret.count(target - nums[i])) {
+                return {ret[target - nums[i]], i};
+            }
+            ret[nums[i]] = i;
+        }
+        assert(0);
     }
 };
-
-
-#endif //PRACTISE_SOLUTION_H
