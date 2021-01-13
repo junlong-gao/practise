bd59c55ab3a63bcf6f7a9d81b76c5ceadb06ca74
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-12 23:32:45 -0800

    Update 767.cpp

--- code_practise/leetcode/767.cpp
+++ code_practise/leetcode/767.cpp
@@ -1,44 +1,3 @@
-// Take 1: naive
-class Solution {
-public:
-    string reorganizeString(string S) {
-      string t = "";
-      for (int i = 0; i < S.size(); ++i) {
-
-        for (int j = 0; j + 1 < t.size(); ++j) {
-          if (t[j] == t[j+1] && S[i] != t[j]) {
-            t.insert(j + 1, S.substr(i, 1));
-            break;
-          } 
-        }
-        if (t.size() == i + 1)  continue;
-        
-        for (int j = 0; j + 1 < t.size(); ++j) {
-          if (S[i] != t[j+1] && S[i] != t[j]) {
-            t.insert(j + 1, S.substr(i, 1));
-            break;
-          } 
-        }
-        if (t.size() == i + 1)  continue;
-        
-      
-          if (S[i] != t[0]) {
-            t.insert(0, S.substr(i, 1));
-          } else if (S[i] != t.back()) {
-            t.push_back(S[i]);
-          } else {
-            t.push_back(S[i]);
-          }
-        
-      }
-      for (int i = 1; i < S.size(); ++i) {
-        if (t[i] == t[i-1]) return "";
-      }
-      return t;
-    }
-};
-
-// Take 2: greedy
 // Only take the most frequent 2 char sofar and append them to the result;
 // it is important to show that this construction cannot introduce adjacent repetition
 // let X Y be current top 2, Z is the 3rd. when their is a count tie, use char X Y Z order to break tie
@@ -53,74 +12,56 @@ public:
 //              if Z.count < (X.count = Y.count) we get X Y
 // In any case, it is impossible to have Y after X Y
 // when the queue has only 1 element, we append it to ret and check repetition.
-// repetition happens when the last element has count >= 2 or repeat against the last of the current
-// constructed ret string.
-
-  string reorganizeString(string S) {
-    unordered_map<char, int> counts;
-    for (auto c : S) {
-      counts[c] ++;
-    }
-    auto comp = [](pair<char, int> &l, pair<char, int> &r) {
-      if (l.second != r.second) {
-        return l.second < r.second;
-      }
-      return l.first < r.first;
+class Solution {
+    struct ent_t {
+        int count;
+        char c;
     };
-    priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(comp)> q(counts.begin(), counts.end(), comp);
-    string ret;
-    while(q.size() >= 2){
-      auto f = q.top(); q.pop();
-      auto s = q.top(); q.pop();
-      ret.push_back(f.first); f.second--;
-      ret.push_back(s.first); s.second--;
-      if (f.second > 0) q.push(f);
-      if (s.second > 0) q.push(s);
+public:
+    string reorganizeString(string S) {
+        auto cmp = [](const ent_t &l, const ent_t &r) {
+           return l.count < r.count;  
+        };
+        priority_queue<ent_t, vector<ent_t>, decltype(cmp)> q(cmp);
+        unordered_map<char, int> refcount;
+        for (auto c : S) {
+            refcount[c]++;
+        }
+        for (auto pr : refcount) {
+            q.push(ent_t{pr.second, pr.first});
+        }
+        
+        int count = 0;
+        string ret;
+        char last = 0;
+        while (q.size()) {
+            auto top = q.top();
+            q.pop();
+            if (ret.size() && ret.back() == top.c) {
+                return "";
+            }
+            ret.push_back(top.c);
+            top.count--;
+            if (q.empty()) {
+                if (top.count == 0) {
+                    break;
+                } else {
+                    return "";
+                }
+            }
+            auto secTop = q.top();
+            q.pop();
+            assert(secTop.c != top.c);
+            ret.push_back(secTop.c);
+            secTop.count--;
+            if (top.count > 0) {
+                q.push(top);
+            }
+            if (secTop.count > 0) {
+                q.push(secTop);
+            }
+        }
+        
+        return ret;
     }
-    if (!q.empty()) {
-      auto t = q.top(); q.pop();
-      if (t.second >= 2 || (!ret.empty() && ret.back() == t.first))  return "";
-      ret.push_back(t.first);
-    }
-    return ret;
-  }
-  
-int main() {
-  
-  {
-  Solution s;
-  cout << s.reorganizeString("cxmwmmm") << endl;; // mxmwmcm
-}
-{
-  Solution s;
-  cout << s.reorganizeString("baaba") << endl;; // ababa
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aaa") << endl;; //
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aab") << endl;; // aba
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aabc") << endl;; // abac
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aaac") << endl;; //
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aaba") << endl;; //
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aabac") << endl;; //abaca
-}
-{
-  Solution s;
-  cout << s.reorganizeString("aaabc") << endl;; //
-}
-}
+};
