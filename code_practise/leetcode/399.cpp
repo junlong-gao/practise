8a443296c203c142cbe9f7d1ad858665126f3290
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-12 23:35:47 -0800

    Update 399.cpp

--- code_practise/leetcode/399.cpp
+++ code_practise/leetcode/399.cpp
@@ -1,57 +1,47 @@
 class Solution {
-    unordered_map<string,
-       unordered_map<string, double>> g;
-    unordered_map<string, bool> visited;
-
-    bool search(
-       double &sofar,
-       string cur,
-       string dst) {
-       if (cur == dst) return true;
-
-       for (auto &nextPr : g[cur]) {
-           string next = nextPr.first;
-           double w = nextPr.second;
-
-           if (visited[next]) continue;
-
-           visited[next] = true;
-           sofar *= w;
-           if (search(sofar, next, dst)) {
-               return true;
-           }
-
-           sofar /= w;
-           visited[next] = false;
-       }
-
-       return false;
+    struct ent_t {
+        string n;
+        double d;
+    };
+    double search(unordered_map<string, unordered_map<string, double>> &g, string &src, string &dst) {
+        deque<ent_t> q{ent_t{src, 1.0}};
+        unordered_set<string> vis;
+        vis.insert(src);
+        while (q.size()) {
+            int n = q.size();
+            while (n-->0) {
+                auto top = q.front();
+                q.pop_front();
+                if (top.n == dst) {
+                    return top.d;
+                }
+                for (auto n : g[top.n]) {
+                    if (vis.count(n.first)) {
+                        continue;
+                    }
+                    vis.insert(n.first);
+                    q.push_back(ent_t{n.first, top.d * n.second});
+                }
+            }
+        }
+        return -1;
     }
 public:
-    vector<double> calcEquation(
-        vector<vector<string>>& equations,
-        vector<double>& values,
-        vector<vector<string>>& queries) {
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, unordered_map<string, double>> g;
         for (int i = 0; i < equations.size(); ++i) {
             g[equations[i][0]][equations[i][1]] = values[i];
             g[equations[i][1]][equations[i][0]] = 1.0 / values[i];
         }
-
+        
         vector<double> ret;
-        for (auto &q : queries) {
-            visited = unordered_map<string, bool>();
-            for (auto & nPr : g) {
-                visited[nPr.first] = false;
-            }
-            visited[q[0]] = true;
-            double ans = 1;
-            if (g.count(q[0]) && search(ans, q[0], q[1])) {
-                ret.push_back(ans);
-            } else {
-                ret.push_back(-1);
-            }
+        for (auto & qry : queries) {
+           if (g.count(qry[0]) * g.count(qry[1]) == 0) {
+               ret.push_back(-1);
+           } else {
+               ret.push_back(search(g, qry[0], qry[1])); 
+           }
         }
-
         return ret;
     }
 };
