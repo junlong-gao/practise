1086cc1aa032d2101cb8b0bf7b1d9c56b3eb3141
cat_mint <junlong-gao@users.noreply.github.com>
2021-01-18 20:03:56 -0800

    Update 32.cpp

--- code_practise/leetcode/32.cpp
+++ code_practise/leetcode/32.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        vector<int> dp(s.length() + 1, 0);
-        int best = 0;
-        for (int i = 1; i < s.length(); ++i) {
-            if (i == 1) {
-                dp[i] = (s[i] == ')' && s[i-1] == '(') ? 2 : 0;
+        vector<int> left;
+        vector<int> dp(s.size() + 1, 0);
+        int ret = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                left.push_back(i);
+                dp[i] = 0;
             } else {
-                // s[0..i] has at least 3 elements.
-                if (s[i] == '(') {
-                    continue;
-                }
-
-                if (s[i-1] == ')') {
-                    // ... (...))
-                    int pivit = i-1-dp[i-1];
-                    if (pivit < 0) {
-                        continue;
-                    }
-                    if (s[pivit] == ')') {
-                        continue;
-                    }
-                    dp[i] = 2 + dp[i-1] + (pivit > 0 ? dp[pivit - 1] : 0);
+                if (left.empty()) {
+                    dp[i] = 0;
                 } else {
-                    // ... ()
-                    dp[i] = 2 + dp[i-2];
+                    if (left.back() == 0) {
+                        dp[i] = i + 1;
+                    } else {
+                        dp[i] = dp[left.back() - 1] + i - left.back() + 1;
+                    }
+                    left.pop_back();
                 }
             }
-            best = max(best, dp[i]);
+            ret = max(ret, dp[i]);
         }
-        return best;
+        return ret;
     }
 };
