8e917b2287c606a701d0d98b7807b78cc8fcec65
junlong-gao <junlongg@vmware.com>
2021-02-15 04:38:26 +0000

    Mon 15 Feb 2021 04:38:26 AM UTC

--- code_practise/leetcode/207.cpp
+++ code_practise/leetcode/207.cpp
@@ -1,106 +1,37 @@
-/*
- * @lc app=leetcode id=207 lang=cpp
- *
- * [207] Course Schedule
- *
- * https://leetcode.com/problems/course-schedule/description/
- *
- * algorithms
- * Medium (38.74%)
- * Total Accepted:    240.7K
- * Total Submissions: 621.4K
- * Testcase Example:  '2\n[[1,0]]'
- *
- * There are a total of n courses you have to take, labeled from 0 to n-1.
- *
- * Some courses may have prerequisites, for example to take course 0 you have
- * to first take course 1, which is expressed as a pair: [0,1]
- *
- * Given the total number of courses and a list of prerequisite pairs, is it
- * possible for you to finish all courses?
- *
- * Example 1:
- *
- *
- * Input: 2, [[1,0]]
- * Output: true
- * Explanation: There are a total of 2 courses to take.
- * To take course 1 you should have finished course 0. So it is possible.
- *
- * Example 2:
- *
- *
- * Input: 2, [[1,0],[0,1]]
- * Output: false
- * Explanation: There are a total of 2 courses to take.
- * To take course 1 you should have finished course 0, and to take course 0 you
- * should
- * also have finished course 1. So it is impossible.
- *
- *
- * Note:
- *
- *
- * The input prerequisites is a graph represented by a list of edges, not
- * adjacency matrices. Read more about how a graph is represented.
- * You may assume that there are no duplicate edges in the input
- * prerequisites.
- *
- *
- */
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // node1 -> node2 iff take node 1 depends on node 2
-        vector<unordered_set<int>> g(numCourses);
-        // each node's in degree, node 2 in-degree ++
-        vector<int> in(numCourses, 0);
-
-        vector<bool> visited(numCourses, false);
-
-        for (auto & e : prerequisites) {
-           // e: (node2, node1)
-           g[e[1]].insert(e[0]);
-           in[e[0]]++;
+        unordered_map<int, int> indegree;
+        unordered_map<int, vector<int>> g;
+        
+        for (auto & edge : prerequisites) {
+            g[edge[1]].push_back(edge[0]);
+            indegree[edge[0]]++;
         }
-
+        
         deque<int> q;
-        int courseCanTake = 0;
-
-        // initial set of course that you can take
-        for (int n = 0; n < numCourses; ++n) {
-           if (in[n] == 0) {
-              q.push_back(n);
-              visited[n] = true;
-              courseCanTake++;
-           }
+        int count = 0;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push_back(i);
+                count++;
+            }
         }
-
-        while (!q.empty()) {
-           auto n = q.front(); q.pop_front();
-           for (auto & d : g[n]) {
-              if (visited[d]) continue;
-              in[d]--;
-              if (in[d] == 0) {
-                 q.push_back(d);
-                 visited[d] = true;
-                 courseCanTake++;
-              }
-           }
+        
+        while (q.size()) {
+            int sz = q.size();
+            while (sz-->0) {
+                int top = q.front(); q.pop_front();
+                for (auto n : g[top]) {
+                    indegree[n]--;
+                    if (indegree[n] == 0) {
+                        q.push_back(n);
+                        count++;
+                    }
+                }
+            }
         }
-
-        return courseCanTake == numCourses;
+        
+        return count == numCourses;
     }
 };
-
-/*
-TESTS
-2
-[[1,0]]
-2
-[[1,0],[0,1]]
-4
-[[3,0],[0,1], [1, 2]]
-4
-[[3,0],[0,1], [1, 2], [2, 3]]
-*/
--- code_practise/leetcode/440.cpp
+++ code_practise/leetcode/440.cpp
@@ -1,97 +1,45 @@
 class Solution {
-  using ll = long long;
-public:
-		
-  int findKthNumber(int n, int k) {
-     return helper(0, n, k);
-  }
-  
-	// find the kth of this subtree rooted by traversaling path
-	// of sofar, EXCLUDING the subtree root. k == 0 implies take the subtree root,
-  // which implies there is nothing to be done as the caller already appended
-  // the root in sofar, see below recursive call.
-	ll helper(ll sofar, int n, int k) {
-	  if (k == 0) {
-	    return sofar;
-	  }
-	  ll skipCount = 0;
-	  for (int digit = ((sofar == 0)? 1 : 0); digit <= 9; ++digit) {
-	    int acc = skipCount + count(sofar * 10 + digit, n);
-	    if (acc >= k) {
-	      // pick this subtree(digit) and remove all previous skipped subtrees
-	      // as well as the root of this subtree(digit), as the root(digit) is 
-	      // already accounted into sofar <- sofar * 10 + digit
-	      return helper(sofar * 10 + digit, n, k - skipCount - 1);
-	    }
-	    skipCount = acc;
-	  }
-	  assert(0);
-	}
-  
-  // count the total number of the nodes in the subtree rooted by
-  // traversaling a path of prefix, including the subtree root.
-  // count level by level, at a level 0, there is only one node, with value
-  // prefix, the next level has 10: lo = prefix*10 ~ hi = prefix*10 - 1, then the next
-  // level... until we hit n < lo or n < hi && lo <=n, in the first case we know
-  // there is no more level, in the second case there is no more next level, 
-  // and this level's contribution is not the entire hi - lo + 1, rather is
-  // n - lo + 1, as discussed the tree will be complete only except the last level,
-  // which is truncated at a certain point from lo to hi.
-  int count(ll prefix, ll n) {
-    ll count = 0;
-    ll level = 1;
-    while(true) {
-      ll lo = prefix * level;
-      ll hi = lo + level - 1;
-      if (n < lo) {
-        break;
-      }
-      if (n < hi && lo <= n) {
-        count += n - lo + 1;
-      }
-      if (n >= hi) {
-        count += hi - lo + 1;
-      }
-      level *= 10;
+    using ll = long long;
+    
+    // count tree size of prefix whose value <= n
+    ll count(ll prefix, ll n) {
+        ll ret = 0;
+        ll lo = prefix, hi = prefix;
+        while (n >= lo) {
+            ret += min(hi - lo + 1, n - lo + 1);
+            lo = lo * 10;
+            hi = hi * 10 + 9;
+        }
+        return ret;
     }
-    return count;
-  }
-
+    
+    ll search(ll prefix, ll n, ll k) {
+        if (k == 1) {
+            return prefix;
+        }
+        
+        ll skipped = 1; // skip prefix
+        for (ll lastDigit = 0; lastDigit <= 9; ++lastDigit) {
+            if (prefix == 0 && lastDigit == 0) {
+                // no such subtree;
+                continue;
+            }
+            
+            ll nextNode = prefix * 10 + lastDigit;
+            ll subTreeSize = count(nextNode, n);
+            if (k <= skipped + subTreeSize) { 
+                // decend into the node in tree nextnode which has rank: 
+                // [skipped + 1, skipped + subtreesize]
+                return search(nextNode, n, k - skipped);
+            } else {
+                skipped += subTreeSize;
+            }
+        }
+        
+        assert(0);
+    }
+public:
+    int findKthNumber(int n, int k) {
+        return search(0, n, k + 1); // the virtual root has rank 1, so shift by 1
+    }
 };
-
-
-int main() {
-	Solution s;
-	int test = 13;
-	
-	for (int i = 1; i <= test; ++i) {
-      cout << s.findKthNumber(test, i) << endl;
-	}
-	
-	cout << "@@" << endl;
-	test = 9;
-	for (int i = 1; i <= test; ++i) {
-      cout << s.findKthNumber(test, i) << endl;
-	}
-	
-	cout << "@@" << endl;
-	test = 999;
-	for (int i = 1; i <= test; ++i) {
-      cout << s.findKthNumber(test, i) << endl;
-	}
-	
-	cout << "@@" << endl;
-	test = 9999;
-	for (int i = 1; i <= test; ++i) {
-      cout << s.findKthNumber(test, i) << endl;
-	}
-	cout << "@@" << endl;
-	
-
-
-	test = 4897764;
-	for (int i = 3510562; i <= 3510564; ++i) {
-      cout << s.findKthNumber(test, i)  << endl;
-	}
-	// 4159503	
-}
